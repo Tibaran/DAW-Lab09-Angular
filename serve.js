8781c8c8ac0447d2c1f9dab2c937a329c3d03d25
Tibaran <palacios.tibaran@gmail.com>
2021-10-21 00:06:48 -0500

    added express

--- serve.js
+++
@@ -1,7 +0,0 @@
-const child_process = require('child_process');
-
-const DEV_SERVER_PORT = process.env.PORT || 4200;
-
-const child = child_process.exec(`ng serve --port=${DEV_SERVER_PORT}`);
-child.stderr.on('data', err => console.error(err));
-child.stdout.on('data', data => console.log(data.toString()));
